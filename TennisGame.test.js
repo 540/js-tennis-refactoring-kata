8ada59dff01504986d85a02d2d831331c1e46c94
Pablo Albizu <pablo@540deg.com>
2018-04-26 00:12:52 +0200

    Third iteration with all tests

--- TennisGame.test.js
+++ TennisGame.test.js
@@ -1,11 +1,67 @@
 import { default as TennisGame } from './TennisGame1';
 
-describe('tennis game', function() {
-  it('scores fifteen-all', () => {
-    const tennisGame = new TennisGame();
-    tennisGame.wonPoint('player1');
-    tennisGame.wonPoint('player2');
+const allScores = [
+  [0, 0, "Love-All"],
+  [1, 1, "Fifteen-All"],
+  [2, 2, "Thirty-All"],
+  [3, 3, "Deuce"],
+  [4, 4, "Deuce"],
 
-    expect(tennisGame.getScore()).toEqual('Fifteen-All');
+  [1, 0, "Fifteen-Love"],
+  [0, 1, "Love-Fifteen"],
+  [2, 0, "Thirty-Love"],
+  [0, 2, "Love-Thirty"],
+  [3, 0, "Forty-Love"],
+  [0, 3, "Love-Forty"],
+  [4, 0, "Win for player1"],
+  [0, 4, "Win for player2"],
+
+  [2, 1, "Thirty-Fifteen"],
+  [1, 2, "Fifteen-Thirty"],
+  [3, 1, "Forty-Fifteen"],
+  [1, 3, "Fifteen-Forty"],
+  [4, 1, "Win for player1"],
+  [1, 4, "Win for player2"],
+
+  [3, 2, "Forty-Thirty"],
+  [2, 3, "Thirty-Forty"],
+  [4, 2, "Win for player1"],
+  [2, 4, "Win for player2"],
+
+  [4, 3, "Advantage player1"],
+  [3, 4, "Advantage player2"],
+  [5, 4, "Advantage player1"],
+  [4, 5, "Advantage player2"],
+  [15, 14, "Advantage player1"],
+  [14, 15, "Advantage player2"],
+
+  [6, 4, "Win for player1"],
+  [4, 6, "Win for player2"],
+  [16, 14, "Win for player1"],
+  [14, 16, "Win for player2"]
+];
+
+const checkScore = function(player1Score, player2Score) {
+  const game = new TennisGame();
+  const highestScore = Math.max(player1Score, player2Score);
+
+  for (let point = 0; point < highestScore; point++) {
+    if (point < player1Score) {
+      game.wonPoint('player1');
+    }
+
+    if (point < player2Score) {
+      game.wonPoint('player2');
+    }
+  }
+
+  return game.getScore();
+};
+
+describe('tennis game', () => {
+  allScores.forEach((score) => {
+    it(score[2], () => {
+      expect(checkScore(score[0], score[1])).toEqual(score[2]);
+    });
   });
 });
