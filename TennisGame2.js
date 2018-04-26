fadec48c358415cfcdb824b7d316c6a5f8591344
Gorka <gorka@540deg.com>
2018-04-26 13:43:16 +0200

    Implementation aditional iteration

--- TennisGame2.js
+++ TennisGame2.js
@@ -1,13 +1,15 @@
 var TennisGame2 = function (player1Name = 'player1', player2Name = 'player2') {
     this.player1 = {
-        points: 0
+        points: 0,
+        name: player1Name
     };
     this.player2 = {
-        points: 0
+        points: 0,
+        name: player2Name
     };
 };
 
-const individualResult = player => {
+const playingResult = (player1, player2) => {
     const result = {
         0: "Love",
         1: "Fifteen",
@@ -15,7 +17,7 @@ const individualResult = player => {
         3: "Forty"
     };
 
-    return result[player.points]
+    return `${result[player1.points]}-${result[player2.points]}`
 };
 
 const equalityResult = player => {
@@ -28,40 +30,49 @@ const equalityResult = player => {
     return result[player.points] || "Deuce";
 };
 
+const advantageResult = (player1, player2) => {
+    const advantage = hasAdvantage(player1).over(player2) ? player1 : player2;
+
+    return `Advantage ${advantage.name}`;
+};
+
+const winnerResult = (player1, player2) => {
+    const winner = hasWon(player1).to(player2) ? player1 : player2;
+
+    return `Win for ${winner.name}`
+};
+
+const isThereEquality = (player1, player2) =>
+    player1.points === player2.points;
+
 const hasAdvantage = (player1) => ({
     over: player2 => player1.points > player2.points && player2.points >= 3 && (player1.points - player2.points) === 1
 });
+
+const isThereAdvantage = (player1, player2) =>
+    hasAdvantage(player1).over(player2) || hasAdvantage(player2).over(player1)
 
 const hasWon = (player1) => ({
     to: player2 => player1.points >= 4 && (player1.points - player2.points) >= 2
 });
 
-const isEquality = (player1, player2) =>
-    player1.points === player2.points;
-
+const isThereAWinner = (player1, player2) =>
+    hasWon(player1).to(player2) || hasWon(player2).to(player1)
 
 TennisGame2.prototype.getScore = function () {
-    if (isEquality(this.player1, this.player2)) {
+    if (isThereEquality(this.player1, this.player2)) {
         return equalityResult(this.player1)
     }
 
-    if (hasAdvantage(this.player1).over(this.player2)) {
-        return "Advantage player1";
+    if (isThereAdvantage(this.player1, this.player2)) {
+        return advantageResult(this.player1, this.player2);
     }
 
-    if (hasAdvantage(this.player2).over(this.player1)) {
-        return "Advantage player2";
+    if (isThereAWinner(this.player1, this.player2)) {
+        return winnerResult(this.player1, this.player2);
     }
 
-    if (hasWon(this.player1).to(this.player2)) {
-        return "Win for player1";
-    }
-
-    if (hasWon(this.player2).to(this.player1)) {
-        return "Win for player2";
-    }
-
-    return individualResult(this.player1) + "-" + individualResult(this.player2);
+    return playingResult(this.player1, this.player2)
 };
 
 TennisGame2.prototype.P1Score = function () {
